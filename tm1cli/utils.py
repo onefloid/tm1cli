4b067171e324ff5eded727c6f0ec95fc40f82d13
onefloid <33193059+onefloid@users.noreply.github.com>
2024-11-24 22:52:42 +0100

    Fix: Format error message

--- tm1cli/utils.py
+++ tm1cli/utils.py
@@ -1,4 +1,5 @@
 import typer
+from rich import print
 
 
 def resolve_database(ctx: typer.Context, database_name: str) -> dict:
@@ -8,10 +9,10 @@ def resolve_database(ctx: typer.Context, database_name: str) -> dict:
     """
     if not database_name:
         return ctx.obj.get("default_db_config")
-    
     configs = ctx.obj.get("configs")
     if database_name not in configs:
-        typer.echo(f"Error: Database '{database_name}' not found in configuration file: databases.yaml.")
+        print(
+            f"[bold red]Error: Database '{database_name}' not found in configuration file: databases.yaml.[/bold red]"
+        )
         raise typer.Exit(code=1)
-    
     return configs[database_name]
